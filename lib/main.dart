6c8208c4d1ad33b7ddbacf91695c2957fd5a0fcd
M-J-Christina213 <m.j.christina213@gmail.com>
2025-06-06 22:27:59 +0530

    🌙 Added system based dark mode detection 😁

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:swiftdine_app/screens/splash_screen.dart';
 import 'themes/app_theme.dart';
+import 'screens/login_screen.dart';
+import 'screens/home_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const SwiftDineApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class SwiftDineApp extends StatelessWidget {
+  const SwiftDineApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'swiftdine App',
+        title: 'Swiftdine App',
         theme: AppTheme.lightTheme,
-        darkTheme: AppTheme.darkTheme,
+        darkTheme: ThemeData(),
         themeMode: ThemeMode.system, 
         home: SplashScreen(),
         debugShowCheckedModeBanner: false,
-      
 
     );
   }
